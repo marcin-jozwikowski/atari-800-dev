a5edee9408fc64ac23e329c0658b4d82f8f6f1bb
Marcin Jóźwikowski <marcin@jozwikowski.pl>
2020-10-14 22:14:50 +0200

    AtariBASIC - clearing autorun line before main program to avoid conflicts

--- AtariBASIC/tools/build.sh
+++ AtariBASIC/tools/build.sh
@@ -1,5 +1,6 @@
-cp $FILE h1_drive/BUILT.BAS && \
-echo "" >> h1_drive/BUILT.BAS && \
-echo "RUN" >> h1_drive/BUILT.BAS && \
-echo "" >> h1_drive/BUILT.BAS && \
+echo "10" > h1_drive/BUILT.BAS
+cat $FILE >> h1_drive/BUILT.BAS
+echo "" >> h1_drive/BUILT.BAS
+echo "RUN" >> h1_drive/BUILT.BAS
+echo "" >> h1_drive/BUILT.BAS
 atari800 -xl -no-autosave-config -basic -H1 h1_drive/ -hreadwrite tools/AutoBAS.atr
