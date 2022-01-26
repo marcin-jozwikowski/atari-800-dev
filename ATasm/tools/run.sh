4faa68a0225c910500130a2a18d7870600ee5c1b
Marcin Jóźwikowski <marcin.jozwikowski@codete.com>
2022-01-26 18:19:13 +0100

    [ATasm] Altirra recognition upgrade

--- ATasm/tools/run.sh
+++ ATasm/tools/run.sh
@@ -4,8 +4,8 @@ rm ~/.wine/altirra/drive_c/Altirra/out -rf
 cp $(pwd)/out ~/.wine/altirra/drive_c/Altirra -fr
 path=${@//out\//C:\\Altirra\\out\\}
 
-if alias altirra 2>/dev/null; then 
-  altirra $path
+if command -v altirra &> /dev/null; then
+  altirra $path  
 else 
   WINEPREFIX=$HOME/.wine/altirra wine C:\\Altirra\\Altirra.exe $path
 fi
